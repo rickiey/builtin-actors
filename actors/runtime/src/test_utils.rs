b491933ac6fbf606122c9d0ec0eb6b65705e929b
vyzo <vyzo@hackzen.org>
2022-04-12 14:40:13 +0300

    fix test_utils

--- actors/runtime/src/test_utils.rs
+++ actors/runtime/src/test_utils.rs
@@ -1117,7 +1117,7 @@ impl Syscalls for MockRuntime {
         exp.result
     }
 
-    fn deploy_actor(&self, _code_cid: &Cid) -> Result<(), Error> {
+    fn deploy_actor(&self, _code_cid: &Cid) -> anyhow::Result<(), anyhow::Error> {
         Ok(())
     }
 
